b152aa2ec428a8d4cc53323f0c22f0ad57c86d2e
Animesh <animeshiit0806@gmail.com>
2025-12-05 20:54:46 +0530

    fixed issues

--- backend/src/main/java/com/codeastras/backend/codeastras/repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeastras.backend.codeastras.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
-@Repository
-public interface RoomRepository extends JpaRepository<Room, UUID> {
-}
