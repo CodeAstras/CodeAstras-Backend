766005a83562846ad1176cffa893f02a3211558d
Animesh <animeshiit0806@gmail.com>
2025-12-22 02:18:45 +0530

    modularity

---
+++ backend/src/main/java/com/codeastras/backend/codeastras/security/ProjectPermission.java
@@ -0,0 +1,11 @@
+package com.codeastras.backend.codeastras.dto;
+
+public enum ProjectPermission {
+    READ_TREE,
+    READ_FILE,
+    CREATE_FILE,
+    CREATE_FOLDER,
+    UPDATE_FILE,
+    DELETE_FILE,
+    EXECUTE_CODE
+}
---
+++ backend/src/main/java/com/codeastras/backend/codeastras/service/ProjectAccessManagerImpl.java
@@ -0,0 +1,4 @@
+package com.codeastras.backend.codeastras.service;
+
+public class ProjectAccessManagerImpl {
+}
