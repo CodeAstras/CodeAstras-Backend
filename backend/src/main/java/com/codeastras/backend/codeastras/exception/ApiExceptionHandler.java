bc5a78b6f2cd675b09f59d99a26f2b7226b53003
Animesh <animeshiit0806@gmail.com>
2025-12-22 14:09:53 +0530

    fixed

---
+++ backend/src/main/java/com/codeastras/backend/codeastras/exception/ApiExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.codeastras.backend.codeastras.exception;
+
+public class ApiExceptionHandler extends RuntimeException {
+  public ApiExceptionHandler(String message) {
+    super(message);
+  }
+}
