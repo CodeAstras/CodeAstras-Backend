b1b8776f281ed5fa680a6a41edaf245dba413c33
Animesh <animeshiit0806@gmail.com>
2026-01-17 09:16:27 +0530

    removed duplicate src

--- src/main/java/com/codeastras/backend/codeastras/mapper/RoomMapper.java
+++
@@ -1,6 +0,0 @@
-        r.setId(room.getId());
-        r.setName(room.getName());
-        r.setCreatedBy(room.getCreatedBy().getId());
-        r.setCreatedAt(room.getCreatedAt().toInstant());
-        r.setIsActive(room.isActive());
-        r.setMembers(
